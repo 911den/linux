ebad215366f5022258e980bff14e6d50296bae0d
den <47458978+911den@users.noreply.github.com>
2019-02-08 20:56:27 +0000

    Update epel.sh

--- epel.sh
+++ epel.sh
@@ -2,3 +2,4 @@
 string1=$(yum install -y epel-release)
 string2=$(yum update -y)
 string3=$(yum groupinstall 'development tools')
+string4=$(yum  install -y yum-utils net-tools bind-utils htop iftop lsof curl traceroute bzip gdisk httpd httpd-tools httpd-devel mc iptables-services  mariadb mariadb-server mariadb-client nano)
